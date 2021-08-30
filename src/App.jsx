12399f884f8734665229c59850310cdd89a2c1c7
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2021-08-30 09:49:15 +0200

    FEAT useCounter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+
+const useCounter = () => {
+  const [value, setValue] = useState(0);
+
+  const increase = () => {
+    setValue(value + 1);
+  };
+
+  const decrease = () => {
+    setValue(value - 1);
+  };
+
+  const reset = () => {
+    setValue(0);
+  };
+
+  return {
+    value,
+    increase,
+    decrease,
+    reset,
+  };
+};
+
+const App = () => {
+  const counter = useCounter();
   return (
     <div className="App">
-      
+      <div className="counter">
+        <div>{counter.value}</div>
+        <button onClick={counter.increase}>+</button>
+        <button onClick={counter.decrease}>-</button>
+        <button onClick={counter.reset}>reset</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
